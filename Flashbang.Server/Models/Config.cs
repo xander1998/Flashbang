c99b380ff9823de5450bfcc4e2e7a7a31c1653cc
EddieK96 <69137506+EddieK96@users.noreply.github.com>
2021-10-14 14:03:26 +0200

    Add files via upload

--- Flashbang.Server/Models/Config.cs
+++ Flashbang.Server/Models/Config.cs
@@ -16,7 +16,12 @@ namespace Flashbang.Server.Models
         public int AfterTime { get; protected set; } = 8;
         [JsonProperty("range")]
         public float Range { get; protected set; } = 8f;
-
+        [JsonProperty("lethal")]
+        public bool Lethal { get; protected set; } = true; 
+        [JsonProperty("lethal")]
+        public int Damage { get; protected set; } = 25; 
+        [JsonProperty("lethalrange")]
+        public float LethalRange { get; protected set; } = 1.6f; 
         public Config() { }
     }
 }
